272b3d20b98871ce095c6be074234ac0f5e57f41
Takeru Ohta <phjgt308@gmail.com>
2019-04-16 12:57:28 +0900

    Add `FixY`

--- src/distributions.rs
+++ src/distributions.rs
@@ -7,3 +7,41 @@ mod normal;
 pub trait Pdf<T> {
     fn pdf(&self, x: &T) -> f64;
 }
+
+#[derive(Debug)]
+pub struct FixY<D> {
+    inner: D,
+    y: f64,
+}
+impl<D> FixY<D>
+where
+    D: Pdf<(f64, f64)>,
+{
+    pub fn new(inner: D, y: f64) -> Self {
+        FixY { inner, y }
+    }
+
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn set_y(&mut self, y: f64) {
+        self.y = y;
+    }
+
+    pub fn inner(&self) -> &D {
+        &self.inner
+    }
+
+    pub fn inner_mut(&mut self) -> &mut D {
+        &mut self.inner
+    }
+}
+impl<D> Pdf<f64> for FixY<D>
+where
+    D: Pdf<(f64, f64)>,
+{
+    fn pdf(&self, &x: &f64) -> f64 {
+        self.inner.pdf(&(x, self.y))
+    }
+}
