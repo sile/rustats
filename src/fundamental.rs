4381942c948c0f6e55ccd39ef2661fc79621ff46
Takeru Ohta <phjgt308@gmail.com>
2019-11-23 21:27:50 +0900

    Fix lint warnings

--- src/fundamental.rs
+++ src/fundamental.rs
@@ -6,9 +6,9 @@ where
     I: ExactSizeIterator<Item = f64> + Clone,
 {
     let n = xs.len() as f64;
-    let sum = xs.clone().into_iter().sum::<f64>();
+    let sum = xs.clone().sum::<f64>();
     let avg = sum / n;
-    let var = xs.into_iter().map(|x| (x - avg).powi(2)).sum::<f64>() / n;
+    let var = xs.map(|x| (x - avg).powi(2)).sum::<f64>() / n;
     var.sqrt()
 }
 
